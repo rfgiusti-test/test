869f6d687a14213eadafdd64327ad440a9ad6a6c
Rafael Giusti <rfgiusti@gmail.com>
2016-02-22 10:06:27 -0300

    Parse option from command line

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	printf("Hello, world!\n");
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			switch (argv[i][1]) {
+			case 'h':
+				printf("Hello, world!\n");
+				break;
+			case 'o':
+				printf("Oi, mundo!\n");
+				break;
+			default:
+				printf("What's -%c?\n", argv[i][1]);
+				return 1;
+			}
+		}
+	}
+
 	return 0;
 }
